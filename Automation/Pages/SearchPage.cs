aeac33b290b16e6ec4fb6accc840ed29c40f91c3
LEAdmin <LEAdmin@DESKTOP-83322GI>
2017-06-02 16:48:12 +0100

    Fixed Search_And_Filter(?)

--- Automation/Pages/SearchPage.cs
+++ Automation/Pages/SearchPage.cs
@@ -10,24 +10,32 @@ namespace Automation
         {
             public static void SubCategories(string subcategory)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[contains(text(), 'Sub-categories')]/..//label/span[contains(text(), '{subcategory}')]")));
                 var subCategory = Driver.Instance.FindElement(By.XPath($".//h3[contains(text(), 'Sub-categories')]/..//label/span[contains(text(), '{subcategory}')]"));
                 subCategory.Click();
             }
 
             public static void Shapes(int shape)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Shapes')]/..//li[{shape}]/label/span")));
                 var shapeFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Shapes')]/..//li[{shape}]/label/span"));
                 shapeFilter.Click();
             }
 
             public static void Styles(int style)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Styles')]/..//li[{style}]/label/span")));
                 var styleFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Styles')]/..//li[{style}]/label/span"));
                 styleFilter.Click();
             }
 
             public static void Themes(int theme)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Themes')]/..//li[{theme}]/label/span")));
                 var themeFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Themes')]/..//li[{theme}]/label/span"));
                 themeFilter.Click();
             }
@@ -54,12 +62,16 @@ namespace Automation
 
                 public void Go()
                 {
+                    WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                    IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".price.ng-pristine.ng-valid:nth-of-type(1)")));
                     var fromInput = Driver.Instance.FindElement(By.CssSelector(".price.ng-pristine.ng-valid:nth-of-type(1)"));
                     fromInput.SendKeys(from);
 
+                    IWebElement toElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".price.ng-pristine.ng-valid:nth-of-type(2)")));
                     var toInput = Driver.Instance.FindElement(By.CssSelector(".price.ng-pristine.ng-valid:nth-of-type(2)"));
                     toInput.SendKeys(to);
 
+                    IWebElement goElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".filter-values-container>.button")));
                     var goButton = Driver.Instance.FindElement(By.CssSelector(".filter-values-container>.button"));
                     goButton.Click();
                 }
@@ -67,30 +79,40 @@ namespace Automation
 
             public static void Brand(int brandNumber)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Brand')]/..//li[{brandNumber}]/label/span")));
                 var brand = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Brand')]/..//li[{brandNumber}]/label/span"));
                 brand.Click();
             }
 
             public static void Colour(int colour)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Colour')]/..//li[{colour}]/label/span")));
                 var colourFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Colour')]/..//li[{colour}]/label/span"));
                 colourFilter.Click();
             }
 
             public static void Material(int material)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Material')]/..//li[{material}]/label/span")));
                 var materialFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Material')]/..//li[{material}]/label/span"));
                 materialFilter.Click();
             }
 
             public static void Finish(int finish)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Finish')]/..//li[{finish}]/label/span")));
                 var finishFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Finish')]/..//li[{finish}]/label/span"));
                 finishFilter.Click();
             }
 
             public static void Range(int range)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Range')]/..//li[{range}]/label/span")));
                 var rangeFilter = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Range')]/..//li[{range}]/label/span"));
                 rangeFilter.Click();
             }
@@ -121,7 +143,7 @@ namespace Automation
         public static void ChangeView(int number)
         {
             WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(60));
-            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName($"view{number}")));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName($"view{number}")));
             var viewStyle = Driver.Instance.FindElement(By.ClassName($"view{number}"));
             viewStyle.Click();
         }
--- Automation/bestandbrown/BAB_SearchPage.cs
+++ Automation/bestandbrown/BAB_SearchPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace Automation
 {
@@ -14,6 +16,8 @@ namespace Automation
         {
             public static void Designer (int designerNumber)
             {
+                WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Designer')]/..//li[{designerNumber}]/label/span")));
                 var designer = Driver.Instance.FindElement(By.XPath($".//h3[@class='filter-header'][contains(text(), 'Designer')]/..//li[{designerNumber}]/label/span"));
                 designer.Click();
             }

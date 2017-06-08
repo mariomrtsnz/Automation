3e5eb0dd61a4d57a20bcd45d7f388ba197dfe204
LEAdmin <LEAdmin@DESKTOP-83322GI>
2017-06-08 11:50:35 +0100

    Fixed _Portfolio_Project tests(?)

--- Automation/Pages/PortfolioProjectPage.cs
+++ Automation/Pages/PortfolioProjectPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace Automation
 {
@@ -6,30 +8,40 @@ namespace Automation
     {
         public static void Picture(int pictureNumber)
         {
+            WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($".//*[@class='overview']/li[{pictureNumber}]/img")));
             var selectPicture = Driver.Instance.FindElement(By.XPath($".//*[@class='overview']/li[{pictureNumber}]/img"));
             selectPicture.Click();
         }
 
         public static void OpenModal()
         {
+            WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("img-main")));
             var openModal = Driver.Instance.FindElement(By.Id("img-main"));
             openModal.Click();
         }
 
         public static void CloseModal()
         {
+            WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".fancybox-item.fancybox-close")));
             var closeModal = Driver.Instance.FindElement(By.CssSelector(".fancybox-item.fancybox-close"));
             closeModal.Click();
         }
 
         public static void NextPicture()
         {
+            WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a.main-image-rotator.next")));
             var nextButton = Driver.Instance.FindElement(By.CssSelector("a.main-image-rotator.next"));
             nextButton.Click();
         }
 
         public static void GoToWeb()
         {
+            WebDriverWait wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("#client-web-link>a")));
             var website = Driver.Instance.FindElement(By.CssSelector("#client-web-link>a"));
             website.Click();
         }
